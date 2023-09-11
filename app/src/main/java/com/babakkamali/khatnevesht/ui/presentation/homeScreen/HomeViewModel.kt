bed61d6c75deb9910f8af52e27d301f41c44580a
babak <Babakkamali21@gmail.com>
2023-09-11 18:01:35 +0330

    fix createNote sync method with server

--- app/src/main/java/com/babakkamali/khatnevesht/ui/presentation/homeScreen/HomeViewModel.kt
+++ app/src/main/java/com/babakkamali/khatnevesht/ui/presentation/homeScreen/HomeViewModel.kt
@@ -51,89 +51,58 @@ class HomeViewModel(application: Application) : AndroidViewModel(application) {
         viewModelScope.launch {
             noteRepo.softDeleteNoteFromRoom(noteModel.id)
         }
-        syncWithServer(false)
+        syncWithServer()
     }
 
-    fun syncWithServer(shouldRecheck: Boolean = true) {
-
+    fun syncWithServer() {
         viewModelScope.launch {
             try {
                 uiState.value = HomeScreenState.Loading
                 Log.d("SYNC", "Starting sync with server")
 
-                // Fetch all notes from the server
-                val serverNotes = noteApiRepo.getAllNotes().data ?: emptyList()
-                Log.d("SYNC", "Fetched ${serverNotes.size} notes from server")
-
+                // Step 1: Fetch all notes initially
+                val serverNotesInitial = noteApiRepo.getAllNotes().data ?: emptyList()
+                val localNotesInitial = noteRepo.getAllNotesWithSoftDeleteFromRoom()
                 val serverDateFormat =
                     SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS'Z'", Locale.getDefault())
 
-                // Fetch all local notes
-                val localNotes = noteRepo.getAllNotesWithSoftDeleteFromRoom()
-                Log.d("SYNC", "Fetched ${localNotes.size} notes from local database")
-
-                // Handle additions and updates
-                for (localNote in localNotes) {
-                    val correspondingServerNote = serverNotes.find { it.id == localNote.serverId }
+                // Step 2: Handle additions and updates
+                for (localNote in localNotesInitial) {
+                    val correspondingServerNote = serverNotesInitial.find { it.id == localNote.serverId }
 
                     if (localNote.serverId == 0) {
                         // New note, create on server
-                        val createResponse = noteApiRepo.createNote(
-                            CreateNoteRequest(
-                                localNote.title,
-                                localNote.notes
-                            )
-                        )
+                        val createResponse = noteApiRepo.createNote(CreateNoteRequest(localNote.title, localNote.notes))
                         localNote.serverId = createResponse.data.id
                         noteRepo.updateNoteInRoom(localNote)
-                        Log.d(
-                            "SYNC",
-                            "Created new note on server with ID: ${createResponse.data.id}"
-                        )
                     } else if (correspondingServerNote != null) {
-                        // Compare updatedAt timestamps
+                        // Compare timestamps
                         val serverDate = serverDateFormat.parse(correspondingServerNote.updatedAt)
                         when {
                             localNote.updatedAt?.compareTo(serverDate) == 1 -> {
                                 // Local note is newer, update on server
-                                noteApiRepo.updateNote(
-                                    localNote.serverId,
-                                    CreateNoteRequest(localNote.title, localNote.notes)
-                                )
-                                Log.d(
-                                    "SYNC",
-                                    "Updated note on server with ID: ${localNote.serverId}"
-                                )
+                                noteApiRepo.updateNote(localNote.serverId, CreateNoteRequest(localNote.title, localNote.notes))
                             }
-
                             localNote.updatedAt?.compareTo(serverDate) == -1 -> {
                                 // Server note is newer, update locally
                                 localNote.title = correspondingServerNote.title
                                 localNote.notes = correspondingServerNote.description
                                 noteRepo.updateNoteInRoom(localNote)
-                                Log.d("SYNC", "Updated local note with ID: ${localNote.id}")
                             }
                         }
                     }
                 }
 
-                // Handle new notes from server and deletions
-                val localNoteServerIds = localNotes.map { it.serverId }.toSet()
-                val softDeletedIds =
-                    localNotes.filter { it.isDeleted == 1 }.map { it.serverId }.toSet()
+                // Step 3: Refetch all server notes to get an updated list
+                val updatedServerNotes = noteApiRepo.getAllNotes().data ?: emptyList()
 
-                for (serverNote in serverNotes) {
-                    Log.e(
-                        "SYNC",
-                        "serverNote.id=" + serverNote.id + "localNoteServerIds.contains=" + localNoteServerIds.contains(
-                            serverNote.id
-                        ) + "oftDeletedIds.contains=" + !softDeletedIds.contains(serverNote.id)
-                    )
-                    if (!localNoteServerIds.contains(serverNote.id) && !softDeletedIds.contains(
-                            serverNote.id
-                        )
-                    ) {
-                        // New note from server, add to local database
+                // Step 4: Handle new notes from the server
+                val updatedLocalNotes = noteRepo.getAllNotesWithSoftDeleteFromRoom()
+                val localNoteServerIds = updatedLocalNotes.map { it.serverId }.toSet()
+                val softDeletedIds = updatedLocalNotes.filter { it.isDeleted == 1 }.map { it.serverId }.toSet()
+
+                for (serverNote in updatedServerNotes) {
+                    if (!localNoteServerIds.contains(serverNote.id) && !softDeletedIds.contains(serverNote.id)) {
                         val newLocalNote = NoteModel(
                             id = 0,
                             title = serverNote.title,
@@ -143,34 +112,24 @@ class HomeViewModel(application: Application) : AndroidViewModel(application) {
                             serverId = serverNote.id
                         )
                         noteRepo.insertNoteToRoom(newLocalNote)
-                        Log.d(
-                            "SYNC",
-                            "Added new note from server with ID: ${serverNote.id} to local database"
-                        )
                     }
                 }
 
-                // Handle deletions
-                val serverNoteIds = serverNotes.map { it.id }.toSet()
-                for (localNote in localNotes) {
+                // Step 5: Handle deletions
+                val serverNoteIds = updatedServerNotes.map { it.id }.toSet()
+                for (localNote in updatedLocalNotes) {
                     if (!serverNoteIds.contains(localNote.serverId) || localNote.isDeleted == 1) {
-                        // Note was deleted on the server or soft-deleted locally
                         if (localNote.isDeleted == 1) {
                             noteApiRepo.deleteNote(localNote.serverId)
-                            Log.d("SYNC", "Deleted note on server with ID: ${localNote.serverId}")
                         }
                         noteRepo.deleteNoteFromRoom(localNote)
-                        Log.d("SYNC", "Deleted local note with ID: ${localNote.id}")
                     }
                 }
-                if (shouldRecheck) {
-                    Log.d("SYNC", "Starting recheck")
-                    syncWithServer(false) // Ensure recheck only happens once
-                }
+
                 uiState.value = HomeScreenState.Success
                 Log.d("SYNC", "Sync completed")
-            } catch (e: NoConnectivityException) {
 
+            } catch (e: NoConnectivityException) {
                 uiState.value = e.message?.let { HomeScreenState.Error(it) }
             } catch (e: Exception) {
                 uiState.value = e.message?.let { HomeScreenState.Error(it) }
